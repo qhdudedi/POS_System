1800b8f5c99b324fd2aadbc66605fdd1fa14460f
qhdudedi <boyoung035@gmail.com>
2023-11-25 00:41:14 +0900

    setting

---
+++ src/test/java/springfinal/pos/PosApplicationTests.java
@@ -0,0 +1,13 @@
+package springfinal.pos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
